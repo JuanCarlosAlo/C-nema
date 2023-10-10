416eaa53f02d0d1b3e533d6792ba11a7af338ff2
JC <jc.alonsome@gmail.com>
2023-10-10 15:42:24 +0200

    footer styles fixed

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -7,21 +7,20 @@ import { MEASUREMENTS } from '../../constants/measurements';
 
 const Footer = () => {
 	return (
-	  <StyledFooter>
-		<Text
-		  text={'Developed by J.C.A.M'}
-		  color={COLORS.MAIN}
-		  fontSize={MEASUREMENTS.FONTS_SIZE.KEY.SUBTITLES}
-		  align={MEASUREMENTS.ALIGN.LEFT}
-		  margin={'0'}
-		  nofullwidth={false}
-		/>
-		<Link target='_blank' to='https://github.com/JuanCarlosAlo/VPA-COMPI'>
-		  <Icon img={'/images/github.svg'} />
-		</Link>
-	  </StyledFooter>
+		<StyledFooter>
+			<Text
+				text={'Developed by J.C.A.M'}
+				color={COLORS.MAIN}
+				fontSize={MEASUREMENTS.FONTS_SIZE.KEY.SUBTITLES}
+				align={MEASUREMENTS.ALIGN.LEFT}
+				margin={'0'}
+				nofullwidth={false}
+			/>
+			<Link target='_blank' to='https://github.com/JuanCarlosAlo/VPA-COMPI'>
+				<Icon img={'/images/github.svg'} alt='github icon' />
+			</Link>
+		</StyledFooter>
 	);
-  };
-  
-  export default Footer;
-  
+};
+
+export default Footer;
